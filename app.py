78b0fa497404b674ce48215bad112bc4bfd63378
Teoh TeikToe <ttt@mensa.org.sg>
2022-07-17 15:11:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,8 @@ def init():
         print(filename)
         # Open the image form working directory
         image = Image.open(file)
-        #file.save(filename) #Heroku no need static
-        #file = open(filename,"r") #Heroku no need static
         model = load_model("Pneumonia")
         img = np.asarray(image)
-        #image = cv2.imread(filename)
-        #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #img = cv2.merge([gray,gray,gray])
         img.resize((150,150,3))
         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
