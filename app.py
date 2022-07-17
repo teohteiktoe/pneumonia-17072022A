1c7d9e426d97d65a681fe3f5b561a49d15f1186d
Teoh TeikToe <ttt@mensa.org.sg>
2022-07-17 11:44:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ def upload_file():
         print("File Received")
         filename = secure_filename(file.filename)
         print(filename)
-        file.save("./static/"+filename) #Heroku no need static
-        file = open("./static/"+filename,"r") #Heroku no need static
+        file.save(filename) #Heroku no need static
+        file = open(filename,"r") #Heroku no need static
         model = load_model("Pneumonia")
-        image = cv2.imread("./static/"+filename)
+        image = cv2.imread(filename)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         img = cv2.merge([gray,gray,gray])
         img.resize((150,150,3))
