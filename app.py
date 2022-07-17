0114f7b465ea61612a222107fdf687a5b9640251
Teoh TeikToe <ttt@mensa.org.sg>
2022-07-17 14:32:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,15 @@ def init():
         print("File Received")
         filename = secure_filename(file.filename)
         print(filename)
-        file.save(filename) #Heroku no need static
-        file = open(filename,"r") #Heroku no need static
+        # Open the image form working directory
+        image = Image.open(filename)
+        #file.save(filename) #Heroku no need static
+        #file = open(filename,"r") #Heroku no need static
         model = load_model("Pneumonia")
-        image = cv2.imread(filename)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img = cv2.merge([gray,gray,gray])
+        data = np.asarray(image)
+        #image = cv2.imread(filename)
+        #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #img = cv2.merge([gray,gray,gray])
         img.resize((150,150,3))
         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
