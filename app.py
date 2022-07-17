441be8002009e8e12f81e4a5960cb665f7f77155
Teoh TeikToe <ttt@mensa.org.sg>
2022-07-17 14:29:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ def init():
     if request.method == 'POST':
         file = request.files['file']
         print("File Received")
-#         filename = secure_filename(file.filename)
-#         print(filename)
-#         file.save(filename) #Heroku no need static
-#         file = open(filename,"r") #Heroku no need static
-#         model = load_model("Pneumonia")
-#         image = cv2.imread(filename)
-#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         img = cv2.merge([gray,gray,gray])
-#         img.resize((150,150,3))
-#         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
-#         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
-#         pred=model.predict(img)
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save(filename) #Heroku no need static
+        file = open(filename,"r") #Heroku no need static
+        model = load_model("Pneumonia")
+        image = cv2.imread(filename)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img = cv2.merge([gray,gray,gray])
+        img.resize((150,150,3))
+        img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
+        img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
+        pred=model.predict(img)
         pred = "YES"
         return(render_template("index.html", result="YES"))
     else:
