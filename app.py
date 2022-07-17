26fdd7e70823e083e193c18666190334a6ef1d64
Teoh TeikToe <ttt@mensa.org.sg>
2022-07-17 13:22:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,14 @@ def upload_file():
         file.save(filename) #Heroku no need static
         file = open(filename,"r") #Heroku no need static
         model = load_model("Pneumonia")
-        image = cv2.imread(filename)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img = cv2.merge([gray,gray,gray])
-        img.resize((150,150,3))
-        img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
-        img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
-        pred=model.predict(img)
+#         image = cv2.imread(filename)
+#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#         img = cv2.merge([gray,gray,gray])
+#         img.resize((150,150,3))
+#         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
+#         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
+#         pred=model.predict(img)
+        pred = "YES"
         return(render_template("index.html", result=str(pred)))
     else:
         return(render_template("index.html", result="WAITING"))
